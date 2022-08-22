f2716a8ed4fa8d32f0740479532719a7eae4b1c8
Simangaliso Mncwango <mncwango.simangaliso@gmail.com>
2022-08-22 13:29:58 +0200

    first api call

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Navbar,Container} from 'react-bootstrap';
+
+const NavbarStyle = {
+  backgroundColor: 'lightblue'
+}
+
+const Header = ({title}) => {
+    return(
+        <Navbar style={NavbarStyle}  variant="light">
+          <Container>
+          <Navbar.Brand href="#/">{title}</Navbar.Brand>
+          </Container>
+      </Navbar>
+    
+    )
+}
+
+export default Header;
